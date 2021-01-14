f91c11ceef14be13961c251b12a88d08a891462d
Вадим Бабичев <WineCooler@yandex.ru>
2021-01-14 20:24:51 +0300

    Созданы компоненты (без логики и стилей)

---
+++ src/components/post-list-item/post-list-item.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const PostListItem = () => {
+    return (
+        <li className="app-list-item d-flex justify-content-between">
+            <span className = "app-list-item-label">
+                Hello Oleg!
+            </span>
+            <div className="d-flex justify-content-center align-items-center">
+                <button 
+                className="btn-star btm-sm"
+                type="button">
+                    <i className="fa fa-star"></i> 
+                </button>
+                <button 
+                className="btn-trash btm-sm"
+                type="button">
+                    <i className="fa fa-trash-o"></i> 
+                </button>
+                <i className="fa fa-heart"></i> 
+            </div>
+        </li>
+    )
+}
+
+export default PostListItem;
