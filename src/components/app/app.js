28e0ddee2d980f4b47787ab0d616fea6a581fc46
Вадим Бабичев <WineCooler@yandex.ru>
2021-01-15 17:11:01 +0300

    Настроен односторонний датабиндинг + реализованы лайки и избранное

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,17 @@ import PostList from "../post-list"
 import PostAddForm from "../post-add-form";
 
 const App = () => {
+
+    // data - это объект с постами, он может поступать с сервера, например
+    // id (key)- обычно поступают с сервера. Они нужны для того, чтобы при добавлении
+    // нового элемента Реакт понимал какие элементы изменились, а какие осталисть
+    // без изменений и смог перерисовать только изменившиеся.
+    const data = [
+        {label: "Going to learn React", important: true, id: "q"},
+        {label: "That is so good", important: false, id: "e"},
+        {label: "I need a break..", important: false, id: "r"},
+    ]
+
     return (
         <div className="app">
             <AppHeader/>
@@ -16,7 +27,8 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            {/* А здесь мы передаем посты в props */}
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
 
